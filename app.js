aa1a8811c7cbec83994b8ec01d394c6307468426
sami <sami.sillanpaa@iki.fi>
2022-03-26 14:08:12 +0200

    Initial commit. Set up express, mongodb, routes etc.

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database')
+
+// Connect to DB
+mongoose.connect(config.database)
+    //On Connection
+    .then(() => {
+        console.log("Connected to database " + config.database);
+    })
+    //On error
+    .catch((err) =>{
+        console.error('Database error :' + err);
+    });
+
+const app = express();
+
+const users = require('./routes/users');
+
+//Port
+const port = process.env.PORT || 3000;
+
+
+//CORS middleware
+app.use(cors());
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Body parser middleware
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+app.get('/', (req,res) => {
+    res.send('Invalid endpoint');
+})
+
+//Start server
+app.listen(port, () => {
+    console.log('Server started on port: ' + port);
+})
---
+++ routes/users.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user')
+
+//Register
+router.post('/register', (req, res, next) => {
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if(err){
+            res.json({success: false, msg:'Failed to register user'})
+        } else {
+            res.json({success: true, msg: 'User registered'})
+        }
+    });
+
+});
+
+//Auth
+router.post('/authenticate', (req, res, next) => {
+    res.send('AUTH');
+});
+
+//Profile
+router.get('/profile', (req, res, next) => {
+    res.send('Profile');
+});
+
+
+module.exports = router;
